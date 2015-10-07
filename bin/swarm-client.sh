85a6d769f11a2f4e3d78aba479c908692bd4bd00
James Rawlings <jamesrawlings@rely.eclipse.co.uk>
2015-10-07 14:51:19 +0100

    switch to using seperate service so we dont change the existing jenkins service and break others that use it

--- bin/swarm-client.sh
+++ bin/swarm-client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z $JENKINS_MASTER_URL ]; then
-    JENKINS_MASTER_URL=http://jenkins:80
+    JENKINS_MASTER_URL=http://jenkins-agent:80
 fi
 
 java -jar /usr/share/java/swarm-client.jar -disableSslVerification -master ${JENKINS_MASTER_URL} $*
