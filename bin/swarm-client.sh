a9c87637acdd0a8b080dde9406d8bbbf173e9d54
James Rawlings <jamesrawlings@rely.eclipse.co.uk>
2015-10-10 19:06:59 +0100

    add optional labels to lookup swarm clients, also remove hub binary

--- bin/swarm-client.sh
+++ bin/swarm-client.sh
@@ -4,4 +4,8 @@ if [ -z $JENKINS_MASTER_URL ]; then
     JENKINS_MASTER_URL=http://jenkins-agent:80
 fi
 
-java -jar /usr/share/java/swarm-client.jar -disableSslVerification -master ${JENKINS_MASTER_URL} $*
+if [ -z $JENKINS_SWARM_LABEL ]; then
+    JENKINS_SWARM_LABEL=swarm
+fi
+
+java -jar /usr/share/java/swarm-client.jar -disableSslVerification -master ${JENKINS_MASTER_URL} -labels ${JENKINS_SWARM_LABEL} $*
