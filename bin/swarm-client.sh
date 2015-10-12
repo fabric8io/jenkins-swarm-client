d7228e7be2592dbe04a0b422e5995d0b59ae70e9
Ioannis Canellos <iocanel@gmail.com>
2015-10-12 20:49:11 +0300

    Add hosntame as label and name prefix

--- bin/swarm-client.sh
+++ bin/swarm-client.sh
@@ -8,4 +8,4 @@ if [ -z $JENKINS_SWARM_LABEL ]; then
     JENKINS_SWARM_LABEL=swarm
 fi
 
-java -jar /usr/share/java/swarm-client.jar -disableSslVerification -master ${JENKINS_MASTER_URL} -labels ${JENKINS_SWARM_LABEL} $*
+java -jar /usr/share/java/swarm-client.jar -disableSslVerification -master ${JENKINS_MASTER_URL} -labels ${JENKINS_SWARM_LABEL} -labels ${HOSTNAME} -name ${HOSTNAME} $*
